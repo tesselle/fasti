d7ab109b3b69208846c0d5cf2ba3e1ae3d6b54bd
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-09-19 16:08:58 +0200

    Update DESCRIPTION

---
+++ R/fasti-package.R
@@ -0,0 +1,32 @@
+#' @details
+#' \tabular{ll}{
+#'  **Package:** \tab fasti \cr
+#'  **Type:** \tab Package \cr
+#'  **Version:** \tab 0.0.0.9000 \cr
+#'  **License:** \tab GPL-3 \cr
+#' }
+#'
+#' @author
+#' **Full list of authors and contributors** (alphabetic order)
+#'
+#' \tabular{ll}{
+#'   Thomas S. Dye \tab *University of Hawai'i at Manoa, USA* \cr
+#'   Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
+#'   Anne Philippe \tab *Université de Nantes, France* \cr
+#' }
+#'
+#' **Package maintainer**
+#'
+#'  Anne Philippe\cr
+#'  \email{anne.philippe@@univ-nantes.fr}
+#'
+#'  Laboratoire de Mathématiques Jean Leray (UMR 6629)\cr
+#'  2, rue de la Houssinière\cr
+#'  BP 92208\cr
+#'  F-44322 Nantes Cedex 3\cr
+#'  France
+#' @name fasti-package
+#' @aliases fasti
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
